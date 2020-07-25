f0b69592f352809b1233e0a2fbdcce9e221e105b
Priyesh <priyesh321988@gmail.com>
2020-07-25 12:30:13 -0400

    EntityFrameWorkInClass

---
+++ ConsoleApp11/Migrations/20200725153419_InitialMigration.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp11.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeacherId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Subject = table.Column<string>(nullable: true),
+                    StudentPocoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Courses_Students_StudentPocoId",
+                        column: x => x.StudentPocoId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marks",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(nullable: false),
+                    StudentId = table.Column<int>(nullable: false),
+                    Score = table.Column<int>(nullable: false),
+                    MaxScore = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marks", x => new { x.CourseId, x.StudentId });
+                    table.ForeignKey(
+                        name: "FK_Marks_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Marks_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_StudentPocoId",
+                table: "Courses",
+                column: "StudentPocoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_TeacherId",
+                table: "Courses",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Marks_StudentId",
+                table: "Marks",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marks");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleApp11
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        
+    }
+
+    public class BaseLogic<T>
+    {
+        private SchoolContext _context;
+
+        public BaseLogic()
+        {
+            _context = new SchoolContext();
+        }
+        public  void create()
+        {
+        }
+        public  void update()
+        {
+
+        }
+        public  void Delete<T>(T item)
+        {
+        }
+        public IList<T> getAll()
+        {
+            return null;
+        }
+    }
+
+    public class SchoolContext : DbContext
+    {
+
+        public DbSet<StudentPoco> Students { get; set; }
+        public DbSet<CoursePoco> Courses { get; set; }
+        public DbSet<TeacherPoco> Teachers { get; set; }
+        public DbSet<GradePoco> Marks { get; set; }
+
+        protected override void
+            OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-BILBHHR0\HUMBERBRIDGING;Initial Catalog=HUMBER_MARKS_DB;Integrated Security=True;");
+        }
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GradePoco>(
+                entity =>
+                entity.HasKey(entity =>
+                  new { entity.CourseId, entity.StudentId }));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class StudentPoco
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public List<CoursePoco> Courses { get; set; }
+    }
+
+    public class CoursePoco
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+
+        [ForeignKey("TeacherId")]
+        public TeacherPoco Teacher { get; set; }
+
+    }
+
+    public class TeacherPoco
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+
+    public class GradePoco
+    {
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public CoursePoco Course { get; set; }
+        public StudentPoco Student { get; set; }
+        public int Score { get; set; }
+        public int MaxScore { get; set; }
+
+    }
+}
